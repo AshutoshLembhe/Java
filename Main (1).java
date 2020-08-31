334bca424111212724da85b6043c2e99f9d2f8be
Ashutosh Lembhe <ashutoshlembhe50@gmail.com>
2020-08-31 15:00:05 +0530

    Add files via upload

--- Main (1).java
+++ Main (1).java
@@ -5,26 +5,22 @@
 Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
-
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		String name="Ashutosh";
-		String last=" Lembhe";
-		System.out.println(name+last);
-		int num=10,num1=20;
-		int num3=num+num1;
-		System.out.println(num3);
-		byte mynum = 50; // cannot print number exceeding 127
-		System.out.println(mynum);
-		short mynum1=5000; // cannot print number exceeding 32767
-		System.out.println(mynum1);
-		float no=1.5f;
-		// note float no. should end with f.
-		System.out.println(no);
-		double no1=19.9d;
-		// note double no should end with d.
-		System.out.println(no1);
+		System.out.println("Left sided pyramid Pattern");
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the pattern length");
+		int no=scan.nextInt();
+		for(int i=0;i<no;i++)
+		{
+		    int stars=i+1;
+		    for(int j=no;j>stars;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.print("\n");
+		}
 	}
 }
